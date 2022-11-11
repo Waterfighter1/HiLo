04b669e8c3e3068de207b827c866ed44e4e0c771
PFEIL <vp0568153@otc.edu>
2022-11-11 08:35:17 -0600

    Added to HILo.java

--- HiLo.java
+++ HiLo.java
@@ -1,5 +1,14 @@
 public class HiLo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+      int[] deck = new int[52];
+      String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+      String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "Jack", "Queen", "King"};
+
+
+
+
+
+
     }
-}
+}       
