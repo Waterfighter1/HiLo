51dc71a7f99ebd0944447a5eb6b3b56de01f0add
GERTNER <cg0960394@otc.edu>
2022-11-11 09:36:14 -0600

    Added Logic for H/L

--- HiLo.java
+++ HiLo.java
@@ -37,14 +37,60 @@ public class HiLo {
     }
 
     static void startTurn(int cardNumber) {
+
+        Scanner reader = new Scanner(System.in);
+
         String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
         String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8","9", "10", "Jack", "Queen", "King"};
+        int[] intRanks = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
         String theSuit = suits[cardNumber / 13];
         String theRank = ranks[cardNumber % 13];
 
         System.out.println("You're current card is a " + theRank + " of " + theSuit);
-        System.out.println("What do you think the next card will be? High (H) Low (L) Or Same (S)?");
+
+        Boolean goodAnswer = false;
+
+        char userGuess = 'a';
+        
+        while (!goodAnswer) {
+            System.out.print("\nWhat do you think the next card will be? High (H) Low (L) Or Same (S)? : ");
+            userGuess = reader.next().charAt(0);
+
+            if (userGuess == 'H' || userGuess == 'L' || userGuess == 'S') {
+                goodAnswer = true;
+            }
+            else {
+                System.out.println("Please enter an acceptible value: H L or S");
+            }
+        }
+        // If the current card is greater than the next
+        if (intRanks[cardNumber % 13] < intRanks[(cardNumber + 1) % 13]) {
+            if (userGuess == 'H') {
+                System.out.println("Correct!");
+            }
+            else {
+                System.out.println("Incorrect, you guessed " + userGuess + " but the correct response was H!");
+            }
+        }
+        // If the current card is less than the next
+        else if (intRanks[cardNumber % 13] > intRanks[(cardNumber + 1) % 13]) {
+            if (userGuess == 'L') {
+                System.out.println("Correct!");
+            }
+            else {
+                System.out.println("Incorrect, you guessed " + userGuess + " but the correct response was L!");
+            }
+        }
+        // If the current card is equal to the next
+        else {
+            if (userGuess == 'S') {
+                System.out.println("Correct!");
+            }
+            else {
+                System.out.println("Incorrect, you guessed " + userGuess + " but the correct response was S!");
+            }
+        }
     }
 
     public static void main(String[] args) {
