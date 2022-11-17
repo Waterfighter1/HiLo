da26805a7fc5091e00c94d27096711beb02bd4d9
GERTNER <cg0960394@otc.edu>
2022-11-17 09:50:56 -0600

    Added comments to Card.java

--- Card.java
+++ Card.java
@@ -1,22 +1,24 @@
 public class Card {
+    // Var Bank 
     public String Value;
     public String Suite;
     public int intValue;
     public boolean empty = false;
 
-
+    // Argument Constructor ( The one called in the createDeck Method)
     public Card(String suite, String value, int iValue) {
         this.Value = value;
         this.Suite = suite;
         this.intValue = iValue;
     }
 
+    // No-Arg constructor
     public Card() {
         this.Value = "None";
         this.Suite = "None";
     }
 
-
+    // Print the values of the cards
     public void PrintCard() {
         System.out.println("There is a " + Value + " of " + Suite + "s on the table");
     }
